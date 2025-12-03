88b6a288cee9b1a88c3110416a18621f030bbcb5
gibikeystudio <kkoppok32021@gmail.com>
2025-12-03 18:45:42 +0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: "PoliceBook",
-  description: "Generate foto polisi dengan scene baru",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 min-h-screen text-black">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
